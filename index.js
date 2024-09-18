9276250592e09c60f7a64d2b6345962b72c7ef84
studyspot-io <feedback.studyspot@gmail.com>
2024-09-18 16:44:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+        function toggleMenu() {
+            var links = document.getElementById("myLinks");
+            if (links.style.display === "block") {
+                links.style.display = "none";
+            } else {
+                links.style.display = "block";
+            }
+        }
+        function toggleDropdown() {
+            var dropdown = document.getElementById("dropdown");
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none";
+            } else {
+                dropdown.style.display = "block";
+            }
+        }
+        function translatePage(language) {
+        }
