c64caffe89e1c80b1ec752e098e8ed4d4a7ae8a5
studyspot-io <feedback.studyspot@gmail.com>
2025-02-15 23:25:18 +0530

    update js/index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,33 @@
-        function toggleMenu() {
-            var links = document.getElementById("myLinks");
-            if (links.style.display === "block") {
-                links.style.display = "none";
-            } else {
-                links.style.display = "block";
-                console.log ("dgdg")
-            }
-        }
-        function toggleDropdown() {
-            var dropdown = document.getElementById("dropdown");
-            if (dropdown.style.display === "block") {
-                dropdown.style.display = "none";
-            } else {
-                dropdown.style.display = "block";
-            }
-        }
-        function translatePage(language) {
-        }
-            const cookiePopup = document.getElementById('cookie-popup');
-    const cookieBtn = document.getElementById('cookie-btn');
+function toggleMenu() {
+  var links = document.getElementById("myLinks");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+    console.log ("dgdg")
+  }
+}
+function toggleDropdown() {
+  var dropdown = document.getElementById("dropdown");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
+function translatePage(language) {}
+const cookiePopup = document.getElementById('cookie-popup');
+const cookieBtn = document.getElementById('cookie-btn');
 
-    // Check if the cookie consent is already given
-    if (!localStorage.getItem('cookieConsent')) {
-        setTimeout(() => {
-            cookiePopup.classList.add('show');
-        }, 1000); // Show popup after 1 second
-    }
+// Check if the cookie consent is already given
+if (!localStorage.getItem('cookieConsent')) {
+  setTimeout(() => {
+    cookiePopup.classList.add('show');
+  }, 1000); // Show popup after 1 second
+}
 
-    cookieBtn.addEventListener('click', () => {
-        cookiePopup.classList.remove('show');
-        localStorage.setItem('cookieConsent', 'true');
-    });
+cookieBtn.addEventListener('click', () => {
+  cookiePopup.classList.remove('show');
+  localStorage.setItem('cookieConsent', 'true');
+});
 });
