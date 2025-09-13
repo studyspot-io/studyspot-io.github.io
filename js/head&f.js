7a51c813de8aac3e153ba2f9f4f1a6c470ce77a0
studyspot-io <feedback.studyspot@gmail.com>
2025-09-13 21:36:06 +0530

    update js/head&f.js

--- js/head&f.js
+++ js/head&f.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('https://studyspot-io.github.io/a/head.html')
         .then(response => response.text())
         .then(data => document.getElementById('header-container').innerHTML = data);
-
+//-------------------------------------------
     fetch('https://studyspot-io.github.io/a/footer.html')
         .then(response => response.text())
         .then(data => document.getElementById('footer-container').innerHTML = data);
